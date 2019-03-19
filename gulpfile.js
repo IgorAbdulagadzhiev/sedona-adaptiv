4409a2e8eb2ff43efecd266dc27017fb843d5079
Игорь <igor.abdulagajiev@yandex.ru>
2019-03-19 03:33:33 +0300

    пилю автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,58 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var plumber = require("gulp-plumber");
 var server = require("browser-sync");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require('gulp-svgstore');
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var run = require("run-sequence");
 
 gulp.task("style", function(){
-  gulp.src("sass/style.scss")
+  gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
+});
+
+gulp.task('html', function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('sprite', function() {
+  return gulp.src('source/img/icons/icon-*.svg')
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/img/icons'));
+});
+
+gulp.task("build", function(done) {
+  run("style", "sprite", "html", done);
 });
 
 gulp.task("serve", ["style"], function() {
